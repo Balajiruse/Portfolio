afbde446993df446e61a318e536544aac20ac1ff
Balajiruse <balaji.ruse@gmail.com>
2024-02-02 16:13:13 +0530

    Res

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -11,7 +11,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `http://localhost:3000/static/media/Balaji.1c42ad41764b8a9b7c3e.pdf`
+const resumeLink = `http://localhost:3000/static/media/Balaji.3b16dc24526950efdfb6.pdf`
 
 
 const Resume = () => {
